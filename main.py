e6311af5ff7b85b3a4e3af56889b0102a6283533
adzsx <adzsx>
2023-01-07 13:12:00 +0100

    Svae and Open features

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from faker import Faker
-from random import choice, randint
+from random import choice, randint, randrange
 import sys
+import json
+import datetime
+
 
 args = sys.argv
 faker = Faker()
@@ -27,13 +30,31 @@ IDFPy [options]
 
 Options:
 -s PATH         Person will be stored in JSON data in PAHT
+-o PATH         Open Json data in stored PATH
 """
 
+def all():
+  gender = choice(["m", "f"])
+  age = randint(18, 70)
+  setName = faker.name_male() if gender == "m" else faker.name_female()
+  address = faker.address()
+  email = setName.split(" ")[0] + "." + setName.split(" ")[1] + "@" + choice(["gmail", "gmx", "proton", "aol", "outlook", "protonmail"]) + choice([".com", ".de", ".fr", ".uk", ".ru", ".us"])
 
-gender = choice(["m", "f"])
-age = randint(18, 70)
-name = faker.name_male() if gender == "m" else faker.name_female()
-address = faker.address()
-email = name.split(" ")[0] + "." + name.split(" ")[1] + "@" + choice(["gmail", "gmx", "proton", "aol", "outlook", "protonmail"]) + choice([".com", ".de", ".fr", ".uk", ".ru", ".us"])
+  return {"name": setName, "gender": gender, "age": str(age), "mail": email}
 
-print(f"Name: {name}\nGender: {gender}\nAge: {age}\nEmail: {email}\n ")
+person = all()
+
+if not "-o" in args:
+  print(json.dumps(person, indent=4))
+else:
+  path = args[args.index("-o")+1]
+  with open(path, "r") as f:
+    data = json.load(f)
+    print(json.dumps(data, indent=4))
+
+
+
+if "-s" in args:
+  path = args[args.index("-s")+1]
+  with open(path, "w") as f:
+    json.dump(person, f)
